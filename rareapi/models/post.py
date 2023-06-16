3fbec35bbacd717621302e4a8314c5972e6db6e9
Ryan Bigelow <ryanmbigelow@gmail.com>
2023-06-15 21:29:29 -0500

    Update post.py

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -1,9 +1,14 @@
 from django.db import models
 from .category import Category
-class Post(models.Model):
+from .rare_user import RareUser
 
+
+class Post(models.Model):
+    
+    user_id = models.ForeignKey(RareUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     publication_date = models.DateField()
     header_image_url = models.CharField(max_length=500)
+    approved = models.BooleanField(max_length=50)
