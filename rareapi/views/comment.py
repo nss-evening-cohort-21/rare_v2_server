47f7c0c2a652a50c26964c2f7731e24b81f4c619
Erin Stephens <erebstephens@gmail.com>
2023-06-17 11:34:13 -0500

    checking out new branch

--- rareapi/views/comment.py
+++ rareapi/views/comment.py
@@ -5,9 +5,9 @@ from rest_framework import serializers, status
 from rareapi.models import Comment, Post, RareUser
 
 class CommentView(ViewSet):
-
+    """Comment View"""
     def retrieve(self, request, pk):
-
+        """get single"""
         try:
             comment = Comment.objects.get(pk=pk)
             serializer = CommentSerializer(comment)
@@ -17,16 +17,16 @@ class CommentView(ViewSet):
             status=status.HTTP_404_NOT_FOUND)
 
     def list(self, request):
-
+        """get all"""
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
 
     def create(self, request):
-
+        """create"""
         post = Post.objects.get(pk=request.data["post"])
         author = RareUser.objects.get(pk=request.data["rareUser"])
-        
+
         comment = Comment.objects.create(
             content=request.data["content"],
             created_on=request.data["createdOn"],
@@ -37,25 +37,26 @@ class CommentView(ViewSet):
         return Response(serializer.data)
 
     def update(self, request, pk):
-
+        """update"""
         comment = Comment.objects.get(pk=pk)
         comment.content = request.data["content"]
         comment.created_on = request.data["createdOn"]
-        
+
         post_id = Post.objects.get(pk=request.data["post"])
-        comment.post_id = post_id        
+        comment.post_id = post_id
         author_id = RareUser.objects.get(pk=request.data["rareUser"])
         comment.author_id = author_id
         comment.save()
 
         return Response(None, status=status.HTTP_204_NO_CONTENT)
-        
+
     def destroy(self, request, pk):
+        """delete"""
         comment = Comment.objects.get(pk=pk)
         comment.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 class CommentSerializer(serializers.ModelSerializer):
-
+    """comment serializer"""
     class Meta:
         model = Comment
         fields = ('id', 'post_id', 'author_id', 'content', 'created_on')
