2b856cc076b86ca3cc09852201a6220824ee98c3
Allison Blumenthal <blumen.allison@gmail.com>
2023-06-17 11:31:11 -0400

    added delete function

--- rareapi/views/category.py
+++ rareapi/views/category.py
@@ -64,6 +64,13 @@ class CategoryView(ViewSet):
     
     return Response(None, status=status.HTTP_204_NO_CONTENT)
   
+  def destroy(self, request, pk):
+    """Handle DELETE request for a category"""
+    category = Category.objects.get(pk=pk)
+    category.delete()
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
+  
+  
 class CategorySerializer(serializers.ModelSerializer):
   """JSON serializer for categories"""
   
